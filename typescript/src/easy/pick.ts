48acac790070e13bbc1dc07f90bf19597fa252f8
dainiscamel <juhyueon3@naver.com>
2024-05-23 20:36:10 +0900

    feat: Pick 타입 구현

---
+++ typescript/src/easy/pick.ts
@@ -0,0 +1,38 @@
+/*
+  4 - Pick
+  -------
+  ### 질문
+
+  `T`에서 `K` 프로퍼티만 선택해 새로운 오브젝트 타입을 만드는 내장 제네릭 `Pick<T, K>`을 이를 사용하지 않고 구현하세요.
+*/
+
+/* _____________ 여기에 코드 입력 _____________ */
+
+type MyPick<T, K extends keyof T> = {
+  [P in K]: T[P];
+};
+
+/* _____________ 테스트 케이스 _____________ */
+import type { Equal, Expect } from "@type-challenges/utils";
+
+type cases = [
+  Expect<Equal<Expected1, MyPick<Todo, "title">>>,
+  Expect<Equal<Expected2, MyPick<Todo, "title" | "completed">>>,
+  // @ts-expect-error
+  MyPick<Todo, "title" | "completed" | "invalid">
+];
+
+interface Todo {
+  title: string;
+  description: string;
+  completed: boolean;
+}
+
+interface Expected1 {
+  title: string;
+}
+
+interface Expected2 {
+  title: string;
+  completed: boolean;
+}
