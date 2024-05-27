d64be1820e2881df8782f6a5e0853bbba7ac7afb
dainiscamel <juhyueon3@naver.com>
2024-05-27 21:30:00 +0900

    feat: first of array

---
+++ typescript/src/easy/first-of-array.ts
@@ -0,0 +1,39 @@
+/*
+  14 - First of Array
+  -------
+
+  ### 질문
+
+  배열(튜플) `T`를 받아 첫 원소의 타입을 반환하는 제네릭 `First<T>`를 구현하세요.
+
+  예시:
+
+  ```ts
+  type arr1 = ['a', 'b', 'c']
+  type arr2 = [3, 2, 1]
+
+  type head1 = First<arr1> // expected to be 'a'
+  type head2 = First<arr2> // expected to be 3
+  ```
+*/
+
+/* _____________ 여기에 코드 입력 _____________ */
+
+type First<T extends any[]> = T extends [infer F, ...any[]] ? F : never;
+
+/* _____________ 테스트 케이스 _____________ */
+import type { Equal, Expect } from "@type-challenges/utils";
+
+type cases = [
+  Expect<Equal<First<[3, 2, 1]>, 3>>,
+  Expect<Equal<First<[() => 123, { a: string }]>, () => 123>>,
+  Expect<Equal<First<[]>, never>>,
+  Expect<Equal<First<[undefined]>, undefined>>
+];
+
+type errors = [
+  // @ts-expect-error
+  First<"notArray">,
+  // @ts-expect-error
+  First<{ 0: "arrayLike" }>
+];
